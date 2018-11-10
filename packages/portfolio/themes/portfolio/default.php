164e76c8ef75ae7f5e7fadffa11a8a2aabd10b47
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-11-10 21:52:04 +0100

    adds 'portfolio' section

--- packages/portfolio/themes/portfolio/default.php
+++ packages/portfolio/themes/portfolio/default.php
@@ -25,7 +25,7 @@
 </section>
 
 
-<section class="portfolio bg-lightgrey">
+<!-- <section class="portfolio bg-lightgrey">
     <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
         <div class="portfolio-intro text--align-center">
             <div class="grid__col--8 grid__push--2">
@@ -34,6 +34,166 @@
         </div>
         <div class="portfolio-cases">
 
+        </div>
+    </div>
+</section> -->
+<section class="portfolio bg-grey">
+
+    <div class="cards padding--top-2-steps padding--bottom-2-steps">
+
+        <div class="example-1 card">
+            <div class="wrapper">
+
+                <div class="date">
+                    <span class="day">12</span>
+                    <span class="month">Aug</span>
+                    <span class="year">2016</span>
+                </div>
+
+                <div class="data">
+                    <div class="content">
+                    <span class="author">Jane Doe</span>
+                    <h4 class="title"><a href="#">Boxing icon has the will for a couple more fights</a></h4>
+                    <p class="text">The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.</p>
+                    <label for="show-menu" class="menu-button"><span></span></label>
+                    </div>
+                    <input type="checkbox" id="show-menu" />
+                    <ul class="menu-content">
+                    <li>
+                        <a href="#" class="fa fa-bookmark-o"></a>
+                    </li>
+                    <li><a href="#" class="fa fa-heart-o"><span>47</span></a></li>
+                    <li><a href="#" class="fa fa-comment-o"><span>8</span></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="example-2 card">
+            <div class="wrapper">
+            <div class="header">
+                <div class="date">
+                <span class="day">12</span>
+                <span class="month">Aug</span>
+                <span class="year">2016</span>
+                </div>
+                <ul class="menu-content">
+                <li>
+                    <a href="#" class="fa fa-bookmark-o"></a>
+                </li>
+                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
+                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
+                </ul>
+            </div>
+            <div class="data">
+                <div class="content">
+                <span class="author">Jane Doe</span>
+                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
+                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+                <a href="#" class="button">Read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="example-2 card">
+            <div class="wrapper">
+            <div class="header">
+                <div class="date">
+                <span class="day">12</span>
+                <span class="month">Aug</span>
+                <span class="year">2016</span>
+                </div>
+                <ul class="menu-content">
+                <li>
+                    <a href="#" class="fa fa-bookmark-o"></a>
+                </li>
+                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
+                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
+                </ul>
+            </div>
+            <div class="data">
+                <div class="content">
+                <span class="author">Jane Doe</span>
+                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
+                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+                <a href="#" class="button">Read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="example-2 card">
+            <div class="wrapper">
+            <div class="header">
+                <div class="date">
+                <span class="day">12</span>
+                <span class="month">Aug</span>
+                <span class="year">2016</span>
+                </div>
+                <ul class="menu-content">
+                <li>
+                    <a href="#" class="fa fa-bookmark-o"></a>
+                </li>
+                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
+                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
+                </ul>
+            </div>
+            <div class="data">
+                <div class="content">
+                <span class="author">Jane Doe</span>
+                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
+                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+                <a href="#" class="button">Read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="example-2 card">
+            <div class="wrapper">
+            <div class="header">
+                <div class="date">
+                <span class="day">12</span>
+                <span class="month">Aug</span>
+                <span class="year">2016</span>
+                </div>
+                <ul class="menu-content">
+                <li>
+                    <a href="#" class="fa fa-bookmark-o"></a>
+                </li>
+                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
+                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
+                </ul>
+            </div>
+            <div class="data">
+                <div class="content">
+                <span class="author">Jane Doe</span>
+                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
+                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+                <a href="#" class="button">Read more</a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="about bg-yellow">
+    <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
+        <div class="grid__col--8 grid__col--sm-12">
+            <h3>Hi there!</h3>
+            <p>Thank you for taking some time to visit my website. I promise you won't be bored.</p>
+            <br />
+            <p>My name is Pedro Moreira Maria. I'm a 36 years old Portuguese that moved to Rotterdam in the year of 2010. Back in Portugal, I studied civil engineering, but I've always been a true autodidact and tech-enthusiast, so the swap to a different professional area was just a matter of time.</p>
+            <p>The "coding world" has always fascinated me, and after an initial approach as self-employed web-developer, I've decided to focus entirely on my development whithin this fast growing field of expertise.</p>
+            <br />
+            <p>The potential is limitless, with new programming languages and tools being created every day, but my open mind and quick learning ability, together with the natural enthusiasm of someone who really enjoys the challenge of what's being learned, have helped me taking the first steps into the endless lines of code and tricky syntax errors.</p>
+            <br />
+            <p>My goal is to become an experienced front-end developer and my ambition encourages me to keep learning and stay up to date with the new technologies, namely ReactJ.</p>
         </div>
     </div>
 </section>
