876e4fce4cb5c252691a5a7f4847a0b04ff3c1a8
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-11-12 11:18:04 +0100

    adds styling to 'portfolio' section

--- packages/portfolio/themes/portfolio/default.php
+++ packages/portfolio/themes/portfolio/default.php
@@ -20,50 +20,124 @@
             <p>The potential is limitless, with new programming languages and tools being created every day, but my open mind and quick learning ability, together with the natural enthusiasm of someone who really enjoys the challenge of what's being learned, have helped me taking the first steps into the endless lines of code and tricky syntax errors.</p>
             <br />
             <p>My goal is to become an experienced front-end developer and my ambition encourages me to keep learning and stay up to date with the new technologies, namely ReactJ.</p>
+            <br />
         </div>
     </div>
 </section>
 
 
-<!-- <section class="portfolio bg-lightgrey">
-    <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
-        <div class="portfolio-intro text--align-center">
-            <div class="grid__col--8 grid__push--2">
-                Here you can check some of my work. It's chronogically organized, so it's easy to see the progression over the time.
-            </div>
-        </div>
-        <div class="portfolio-cases">
-
-        </div>
-    </div>
-</section> -->
-<section class="portfolio bg-grey">
-
-    <div class="cards padding--top-2-steps padding--bottom-2-steps">
+<section class="portfolio bg-lightgrey">
+    <div class="cards padding--bottom-2-steps">
 
         <div class="example-1 card">
             <div class="wrapper">
 
                 <div class="date">
-                    <span class="day">12</span>
                     <span class="month">Aug</span>
-                    <span class="year">2016</span>
+                    <span class="year">2018</span>
                 </div>
 
                 <div class="data">
                     <div class="content">
-                    <span class="author">Jane Doe</span>
-                    <h4 class="title"><a href="#">Boxing icon has the will for a couple more fights</a></h4>
-                    <p class="text">The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.</p>
-                    <label for="show-menu" class="menu-button"><span></span></label>
+                        <span class="author">Freelance</span>
+                        <h4 class="title"><a href="#">fleximfoundation.com</a></h4>
+                        <p class="text">For this humanitary project, I was asked to help with the website. The goal here was to provide the end-user enough flexibility to use the CMS in a comfortable way.</p>
+                        <label for="show-menu" class="menu-button"><span></span></label>
                     </div>
+                    
                     <input type="checkbox" id="show-menu" />
                     <ul class="menu-content">
-                    <li>
-                        <a href="#" class="fa fa-bookmark-o"></a>
-                    </li>
-                    <li><a href="#" class="fa fa-heart-o"><span>47</span></a></li>
-                    <li><a href="#" class="fa fa-comment-o"><span>8</span></a></li>
+                        <li><a href="#" class=""><span>PHP</span></a></li>
+                        <li><a href="#" class=""><span>SCSS</span></a></li>
+                        <li><a href="#" class=""><span>HMTL</span></a></li>
+                        <li><a href="#" class=""><span>JavaScript</span></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="example-4 card">
+            <div class="wrapper">
+
+                <div class="date">
+                    <span class="month">March</span>
+                    <span class="year">2018</span>
+                </div>
+
+                <div class="data">
+                    <div class="content">
+                        <span class="author">The Red Corner</span>
+                        <h4 class="title"><a href="#">employerbrandingtool.nl</a></h4>
+                        <p class="text">I was solely responsible for the creation of a new website for a
+                        world-player in isolation</p>
+                        <label for="show-menu-4" class="menu-button"><span></span></label>
+                    </div>
+                    
+                    <input type="checkbox" id="show-menu-4" />
+                    <ul class="menu-content">
+                        <li><a href="#" class=""><span>PHP</span></a></li>
+                        <li><a href="#" class=""><span>SCSS</span></a></li>
+                        <li><a href="#" class=""><span>HMTL</span></a></li>
+                        <li><a href="#" class=""><span>JavaScript</span></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="example-2 card">
+            <div class="wrapper">
+
+                <div class="date">
+                    <span class="month">April</span>
+                    <span class="year">2018</span>
+                </div>
+
+                <div class="data">
+                    <div class="content">
+                        <span class="author">The Red Corner</span>
+                        <h4 class="title"><a href="#">bonnemaman.nl</a></h4>
+                        <p class="text">I was solely responsible for the creation of a new website for a
+                        world-player in isolation</p>
+                        <label for="show-menu-2" class="menu-button"><span></span></label>
+                    </div>
+                    
+                    <input type="checkbox" id="show-menu-2" />
+                    <ul class="menu-content">
+                        <li><a href="#" class=""><span>PHP</span></a></li>
+                        <li><a href="#" class=""><span>SCSS</span></a></li>
+                        <li><a href="#" class=""><span>HMTL</span></a></li>
+                        <li><a href="#" class=""><span>JavaScript</span></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="example-3 card">
+            <div class="wrapper">
+
+                <div class="date">
+                    <span class="month">June</span>
+                    <span class="year">2018</span>
+                </div>
+
+                <div class="data">
+                    <div class="content">
+                        <span class="author">The Red Corner</span>
+                        <h4 class="title"><a href="#">heraklith.nl</a></h4>
+                        <p class="text">I was solely responsible for the creation of a new website for a
+                        world-player in isolation</p>
+                        <label for="show-menu-3" class="menu-button"><span></span></label>
+                    </div>
+                    
+                    <input type="checkbox" id="show-menu-3" />
+                    <ul class="menu-content">
+                        <li><a href="#" class=""><span>PHP</span></a></li>
+                        <li><a href="#" class=""><span>SCSS</span></a></li>
+                        <li><a href="#" class=""><span>HMTL</span></a></li>
+                        <li><a href="#" class=""><span>JavaScript</span></a></li>
                     </ul>
                 </div>
 
@@ -96,93 +170,11 @@
             </div>
             </div>
         </div>
-
-        <div class="example-2 card">
-            <div class="wrapper">
-            <div class="header">
-                <div class="date">
-                <span class="day">12</span>
-                <span class="month">Aug</span>
-                <span class="year">2016</span>
-                </div>
-                <ul class="menu-content">
-                <li>
-                    <a href="#" class="fa fa-bookmark-o"></a>
-                </li>
-                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
-                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                </ul>
-            </div>
-            <div class="data">
-                <div class="content">
-                <span class="author">Jane Doe</span>
-                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
-                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
-                <a href="#" class="button">Read more</a>
-                </div>
-            </div>
-            </div>
-        </div>
-
-        <div class="example-2 card">
-            <div class="wrapper">
-            <div class="header">
-                <div class="date">
-                <span class="day">12</span>
-                <span class="month">Aug</span>
-                <span class="year">2016</span>
-                </div>
-                <ul class="menu-content">
-                <li>
-                    <a href="#" class="fa fa-bookmark-o"></a>
-                </li>
-                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
-                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                </ul>
-            </div>
-            <div class="data">
-                <div class="content">
-                <span class="author">Jane Doe</span>
-                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
-                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
-                <a href="#" class="button">Read more</a>
-                </div>
-            </div>
-            </div>
-        </div>
-
-        <div class="example-2 card">
-            <div class="wrapper">
-            <div class="header">
-                <div class="date">
-                <span class="day">12</span>
-                <span class="month">Aug</span>
-                <span class="year">2016</span>
-                </div>
-                <ul class="menu-content">
-                <li>
-                    <a href="#" class="fa fa-bookmark-o"></a>
-                </li>
-                <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
-                <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                </ul>
-            </div>
-            <div class="data">
-                <div class="content">
-                <span class="author">Jane Doe</span>
-                <h4 class="title"><a href="#">Stranger Things: The sound of the Upside Down</a></h4>
-                <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
-                <a href="#" class="button">Read more</a>
-                </div>
-            </div>
-            </div>
-        </div>
-
+        
     </div>
-
 </section>
 
-<section class="about bg-yellow">
+<section class="about">
     <div class="grid__container padding--top-2-steps padding--bottom-2-steps">
         <div class="grid__col--8 grid__col--sm-12">
             <h3>Hi there!</h3>
